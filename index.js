b7375917a45e6418aa48ef9ff0a3c6fdc7390c1e
TajahH <167078781+TajahH@users.noreply.github.com>
2024-04-16 10:16:28 +0100

    Update index.js
    it finally works!

--- index.js
+++ index.js
@@ -7,13 +7,16 @@
 // The dog's age in dog's years is 73
 
 const readline = require("readline-sync");
-let dogYrInput = parseInt(readline.question(`How old is your dog in human years?: `))
 
-    let humanYears = 2;
-    let firstDogYear = humanYears * 10.5;
-    let laterDogYears = dogYrInput - humanYears
-    let subDogYears = firstDogYear + laterDogYears  * 4;
- 
+    let firstDogYear =  10.5; // the first 2 years are worth 10.5 each
+    let laterDogYears = 4
+    let dogYrInput = parseInt(readline.question(`How old is your dog in human years?: `))
 
-
-console.log(`Your dog is ${dogYrInput} in human years. In dog years, your dog is ${subDogYears}`)
+    if(dogYrInput <=0){
+        console.log("Dog Age is 0")
+    
+    } else if (dogYrInput <= 2 && dogYrInput > 0 ){
+        console.log( `Dog age is ${dogYrInput *firstDogYear} years old in dog years`)
+    } else{
+        console.log(`Dog age is ${(firstDogYear*2) + ((dogYrInput-2)* laterDogYears)} years old in dog years`)
+    } 
